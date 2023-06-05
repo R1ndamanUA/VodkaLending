eb9fbca7c46a7344cecc11fa4702bc0656c00214
R1ndamanUA <123882447+R1ndamanUA@users.noreply.github.com>
2023-06-05 16:12:51 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -86,40 +86,40 @@
 // }
 
 // Cookie and AGE CHECK ==================================================================================
-// !function(a){"function"==typeof define&&define.amd?define(["jquery"],a):a(jQuery)}(function(a){function c(a){return h.raw?a:encodeURIComponent(a)}function d(a){return h.raw?a:decodeURIComponent(a)}function e(a){return c(h.json?JSON.stringify(a):String(a))}function f(a){0===a.indexOf('"')&&(a=a.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,"\\"));try{return a=decodeURIComponent(a.replace(b," ")),h.json?JSON.parse(a):a}catch(c){}}function g(b,c){var d=h.raw?b:f(b);return a.isFunction(c)?c(d):d}var b=/\+/g,h=a.cookie=function(b,f,i){if(void 0!==f&&!a.isFunction(f)){if(i=a.extend({},h.defaults,i),"number"==typeof i.expires){var j=i.expires,k=i.expires=new Date;k.setDate(k.getDate()+j)}return document.cookie=[c(b),"=",e(f),i.expires?"; expires="+i.expires.toUTCString():"",i.path?"; path="+i.path:"",i.domain?"; domain="+i.domain:"",i.secure?"; secure":""].join("")}for(var l=b?void 0:{},m=document.cookie?document.cookie.split("; "):[],n=0,o=m.length;o>n;n++){var p=m[n].split("="),q=d(p.shift()),r=p.join("=");if(b&&b===q){l=g(r,f);break}b||void 0===(r=g(r))||(l[q]=r)}return l};h.defaults={},a.removeCookie=function(b,c){return void 0===a.cookie(b)?!1:(a.cookie(b,"",a.extend({},c,{expires:-1})),!a.cookie(b))}});
+!function(a){"function"==typeof define&&define.amd?define(["jquery"],a):a(jQuery)}(function(a){function c(a){return h.raw?a:encodeURIComponent(a)}function d(a){return h.raw?a:decodeURIComponent(a)}function e(a){return c(h.json?JSON.stringify(a):String(a))}function f(a){0===a.indexOf('"')&&(a=a.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,"\\"));try{return a=decodeURIComponent(a.replace(b," ")),h.json?JSON.parse(a):a}catch(c){}}function g(b,c){var d=h.raw?b:f(b);return a.isFunction(c)?c(d):d}var b=/\+/g,h=a.cookie=function(b,f,i){if(void 0!==f&&!a.isFunction(f)){if(i=a.extend({},h.defaults,i),"number"==typeof i.expires){var j=i.expires,k=i.expires=new Date;k.setDate(k.getDate()+j)}return document.cookie=[c(b),"=",e(f),i.expires?"; expires="+i.expires.toUTCString():"",i.path?"; path="+i.path:"",i.domain?"; domain="+i.domain:"",i.secure?"; secure":""].join("")}for(var l=b?void 0:{},m=document.cookie?document.cookie.split("; "):[],n=0,o=m.length;o>n;n++){var p=m[n].split("="),q=d(p.shift()),r=p.join("=");if(b&&b===q){l=g(r,f);break}b||void 0===(r=g(r))||(l[q]=r)}return l};h.defaults={},a.removeCookie=function(b,c){return void 0===a.cookie(b)?!1:(a.cookie(b,"",a.extend({},c,{expires:-1})),!a.cookie(b))}});
 
-// $('#enter').click(function(){
-//    $('.age_check').hide();
-//     $.cookie('age_check', 'over21', { expires: 7, path: '/'        });
-//    return false;
-// });
+$('#enter').click(function(){
+   $('.age_check').hide();
+    $.cookie('age_check', 'over21', { expires: 7, path: '/'        });
+   return false;
+});
 
-// $(document).ready(function() {
-//     // Проверяем наличие cookie для age_check
-//     if (!$.cookie('age_check')) {
-//       $('.age_check').show();
-//       disableScroll(); // Вызываем функцию для отключения скролла
-//     } else {
-//       $('.age_check').hide();
-//     }
+$(document).ready(function() {
+    // Проверяем наличие cookie для age_check
+    if (!$.cookie('age_check')) {
+      $('.age_check').show();
+      disableScroll(); // Вызываем функцию для отключения скролла
+    } else {
+      $('.age_check').hide();
+    }
 
-//     // Обработчик события при нажатии на кнопку "так"
-//     $('#enter').click(function() {
-//       $('.age_check').hide();
-//       enableScroll(); // Вызываем функцию для включения скролла
-//       $.cookie('age_check', 'over21', { expires: 7, path: '/' });
-//       return false;
-//     });
+    // Обработчик события при нажатии на кнопку "так"
+    $('#enter').click(function() {
+      $('.age_check').hide();
+      enableScroll(); // Вызываем функцию для включения скролла
+      $.cookie('age_check', 'over21', { expires: 7, path: '/' });
+      return false;
+    });
 
-//     // Функция для отключения скролла
-//     function disableScroll() {
-//       $('body').css('overflow', 'hidden');
-//     }
+    // Функция для отключения скролла
+    function disableScroll() {
+      $('body').css('overflow', 'hidden');
+    }
 
-//     // Функция для включения скролла
-//     function enableScroll() {
-//       $('body').css('overflow', 'auto');
-//     }
-//   });
+    // Функция для включения скролла
+    function enableScroll() {
+      $('body').css('overflow', 'auto');
+    }
+  });
 //=========END COOKIE AND AGE===============================================================================================================================================
   
